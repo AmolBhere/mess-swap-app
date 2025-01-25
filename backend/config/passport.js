297c5493639c3e270b055071f45e7637a4540e6b
Amol Bhere <amolbhere2003@gmail.com>
2025-01-25 19:13:49 +0530

    update

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: '/auth/google/callback' || 'https://mess-swap-app-backend.onrender.com/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ email: profile.emails[0].value });
