0ee72b427edadc76c98de88f5f841825aad0cd35
Amol Bhere <amolbhere2003@gmail.com>
2025-05-22 16:09:11 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ io.on('connection', (socket) => {
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('MongoDB connection error:', err));
 
